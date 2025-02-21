68c7c0c53857ace9e36a42aa77b7b64377f5c00b
lenakang <kangnara92@gmail.com>
2025-02-22 00:36:46 +0900

    feet : main > projects

--- src/app/components/Layout/Main/Projects/Projects.tsx
+++ src/app/components/Layout/Main/Projects/Projects.tsx
@@ -1,16 +1,33 @@
 import Link from "next/link";
 import { Container } from "../..";
 import styles from "./projects.module.scss";
+import Image from "next/image";
 
 export default function Projects() {
     return (
         <section id="projects" className={styles.projects}>
             <Container>
-                <div className={styles.projects__inner}>
+                <div className={styles.projects__intro}>
                     <h2>projects</h2>
+                    <Link href="projects">View More →</Link>
                 </div>
-                <Link href="projects">More</Link>
             </Container>
+            <div className={styles.projects__list}>
+                {Array.from({ length: 20 }, (_, i) => (
+                    <Link
+                        href="/"
+                        key={i}
+                        className={styles.projects__list_item}
+                    >
+                        <Image
+                            src="/thumbnails/ooff.jpg"
+                            width="400"
+                            height="400"
+                            alt="thumbnail"
+                        />
+                    </Link>
+                ))}
+            </div>
         </section>
     );
 }
