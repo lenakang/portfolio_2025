5a2370501ec78e589ebbc747d5fd8d7ff1688001
lenakang <kangnara92@gmail.com>
2025-03-18 17:39:53 +0900

    fix : projects page build 에러 수정1

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,23 +3,31 @@ import ProjectList from "../components/ProjectList/ProjectList";
 import db from "../lib/db";
 
 async function getProjects() {
-    return await db.project.findMany({
-        select: {
-            id: true,
-            name: true,
-            photo: true,
-        },
-    });
+  const projects = await db.project.findMany({
+    select: {
+      id: true,
+      name: true,
+      photo: true,
+    },
+  });
+
+  return {
+    props: { projects },
+    revalidate: 60,
+  };
 }
 
-export default async function page() {
-    const projects = await getProjects();
-    return (
-        <Container>
-            <h1>Projects</h1>
-            {projects.map((project) => (
-                <ProjectList key={project.id} {...project} />
-            ))}
-        </Container>
-    );
+export default async function page({
+  projects,
+}: {
+  projects: { id: number; name: string; photo: string }[];
+}) {
+  return (
+    <Container>
+      <h1>Projects</h1>
+      {projects.map((project) => (
+        <ProjectList key={project.id} {...project} />
+      ))}
+    </Container>
+  );
 }
